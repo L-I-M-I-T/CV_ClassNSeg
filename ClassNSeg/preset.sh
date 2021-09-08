c310b7539a23ff60dd10d3e5cbf264dda1159167
limit <wang2000tian@foxmail.com>
2021-09-08 18:48:38 +0800

    First Add

---
+++ ClassNSeg/preset.sh
@@ -0,0 +1,5 @@
+python download++.py datasets/ -d original -c c23 -t videos
+python download++.py datasets/ -d Face2Face -c c23 -t masks
+python download++.py datasets/ -d Face2Face -c c23 -t videos
+
+python create_dataset_Face2Face.py
